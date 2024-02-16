2ddbd41bc6b08d5f5d11f18e77ab1156680b035d
fmac2000 <62116993+fmac2000@users.noreply.github.com>
2024-02-16 14:51:46 +0000

    Fix bs_revnet.py
    Autocast fixes, indentation error and default backward pass for inference

--- models/bs_revnet/bs_revnet.py
+++ models/bs_revnet/bs_revnet.py
@@ -77,8 +77,7 @@ class RevBackProp(Function):
 
         for _, lyr in enumerate(layers):
             X_1, X_2 = lyr(X_1, X_2) # lyr returns Y1, Y2 (inplace to reduce footprint)
-
-        all_tensors = [X_1.detach(), X_2.detach()] # save for backward pass
+            all_tensors = [X_1.detach(), X_2.detach()] # save for backward pass
         ctx.save_for_backward(*all_tensors)
         ctx.layers = layers
 
@@ -228,7 +227,8 @@ class FeedForward(Module):
         )
 
     def forward(self, x):
-        return self.net(x)
+        with torch.cuda.amp.autocast(enabled=True):
+            return self.net(x)
 
 
 class Attention(Module):
@@ -261,21 +261,22 @@ class Attention(Module):
         )
 
     def forward(self, x):
-        x = self.norm(x)
-
-        q, k, v = rearrange(self.to_qkv(x), 'b n (qkv h d) -> qkv b h n d', qkv=3, h=self.heads)
-
-        if exists(self.rotary_embed):
-            q = self.rotary_embed.rotate_queries_or_keys(q)
-            k = self.rotary_embed.rotate_queries_or_keys(k)
-
-        out = self.attend(q, k, v)
-
-        gates = self.to_gates(x)
-        out = out * rearrange(gates, 'b n h -> b h n 1').sigmoid()
-
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
+        with torch.cuda.amp.autocast(enabled=True):
+            x = self.norm(x)
+    
+            q, k, v = rearrange(self.to_qkv(x), 'b n (qkv h d) -> qkv b h n d', qkv=3, h=self.heads)
+    
+            if exists(self.rotary_embed):
+                q = self.rotary_embed.rotate_queries_or_keys(q)
+                k = self.rotary_embed.rotate_queries_or_keys(k)
+    
+            out = self.attend(q, k, v)
+    
+            gates = self.to_gates(x)
+            out = out * rearrange(gates, 'b n h -> b h n 1').sigmoid()
+    
+            out = rearrange(out, 'b h n d -> b n (h d)')
+            return self.to_out(out)
 
 
 class DropPath(Module):
@@ -291,14 +292,15 @@ class DropPath(Module):
     def forward(self, x):
         if not self.training:
             return x
-        
-        keep_prob = 1 - self.drop_prob
-        shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-        random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
-        if keep_prob > 0.0 and self.scale_by_keep:
-            random_tensor.div_(keep_prob)
 
-        return x * random_tensor
+        with torch.cuda.amp.autocast(enabled=True):
+            keep_prob = 1 - self.drop_prob
+            shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+            random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+            if keep_prob > 0.0 and self.scale_by_keep:
+                random_tensor.div_(keep_prob)
+    
+            return x * random_tensor
 
 
 class ReversibleLayer(Module):
@@ -473,7 +475,14 @@ class ReversibleLayer(Module):
 
         return dY_1, dY_2
 
+    def backward_default(self, x, layers)
+        X_1, X_2 = torch.chunk(x, 2, dim=-1) # split the concat X
 
+        for _, lyr in enumerate(layers):
+            X_1, X_2 = lyr(X_1, X_2) # lyr returns Y1, Y2 (inplace to reduce footprint)
+
+        return torch.cat([X_1, X_2], dim=-1) # concat X
+    
 class ReversibleTransformer(Module):
     def __init__(
             self,
@@ -493,7 +502,9 @@ class ReversibleTransformer(Module):
     ):
         super().__init__()
 
-        if fast_backprop:
+        if not self.training:
+            self.executing_fn = ReversibleLayer.backward_default
+        elif fast_backprop:
             global s1, s2
             s1 = torch.cuda.default_stream(device=torch.cuda.current_device())
             s2 = torch.cuda.Stream(device=torch.cuda.current_device())
@@ -795,35 +806,37 @@ class BSRevnet(Module):
         d - feature dimension
         """
 
-        device = raw_audio.device
-
-        if raw_audio.ndim == 2:
-            raw_audio = rearrange(raw_audio, 'b t -> b 1 t')
-
-        channels = raw_audio.shape[1]
-        assert (not self.stereo and channels == 1) or (
-                    self.stereo and channels == 2), 'stereo needs to be set to True if passing in audio signal that is stereo (channel dimension of 2). also need to be False if mono (channel dimension of 1)'
-
-        # to stft
-
-        raw_audio, batch_audio_channel_packed_shape = pack_one(raw_audio, '* t')
-
-        stft_window = self.stft_window_fn(device=device)
-
-        stft_repr = torch.stft(raw_audio, **self.stft_kwargs, window=stft_window, return_complex=True)
-        stft_repr = torch.view_as_real(stft_repr)
-
-        stft_repr = unpack_one(stft_repr, batch_audio_channel_packed_shape, '* f t c')
-        stft_repr = rearrange(stft_repr,
-                              'b s f t c -> b (f s) t c')  # merge stereo / mono into the frequency, with frequency leading dimension, for band splitting
-
-        x = rearrange(stft_repr, 'b f t c -> b t (f c)')
-
-        x = self.band_split(x)
-
-        # axial / hierarchical attention
+        with torch.cuda.amp.autocast(enabled=True):
         
-        x = torch.cat([x, x], dim=-1) # b t f d -> b f t d*2
+            device = raw_audio.device
+    
+            if raw_audio.ndim == 2:
+                raw_audio = rearrange(raw_audio, 'b t -> b 1 t')
+    
+            channels = raw_audio.shape[1]
+            assert (not self.stereo and channels == 1) or (
+                        self.stereo and channels == 2), 'stereo needs to be set to True if passing in audio signal that is stereo (channel dimension of 2). also need to be False if mono (channel dimension of 1)'
+    
+            # to stft
+    
+            raw_audio, batch_audio_channel_packed_shape = pack_one(raw_audio, '* t')
+    
+            stft_window = self.stft_window_fn(device=device)
+    
+            stft_repr = torch.stft(raw_audio, **self.stft_kwargs, window=stft_window, return_complex=True)
+            stft_repr = torch.view_as_real(stft_repr)
+    
+            stft_repr = unpack_one(stft_repr, batch_audio_channel_packed_shape, '* f t c')
+            stft_repr = rearrange(stft_repr,
+                                  'b s f t c -> b (f s) t c')  # merge stereo / mono into the frequency, with frequency leading dimension, for band splitting
+    
+            x = rearrange(stft_repr, 'b f t c -> b t (f c)')
+    
+            x = self.band_split(x)
+
+            # axial / hierarchical attention
+            
+            x = torch.cat([x, x], dim=-1) # b t f d -> b f t d*2
 
         for time_transformer, freq_transformer in self.layers:
             
@@ -839,73 +852,75 @@ class BSRevnet(Module):
             x = freq_transformer(x)
             x, = unpack(x, ps, '* f d')
 
-        x = self.fuse(x) # b f t d*2 -> b t f d
-
-        x = self.final_norm(x) 
-
-        num_stems = len(self.mask_estimators)
-
-        mask = torch.stack([fn(x) for fn in self.mask_estimators], dim=1)
-        mask = rearrange(mask, 'b n t (f c) -> b n f t c', c=2)
-
-        # modulate frequency representation
-
-        stft_repr = rearrange(stft_repr, 'b f t c -> b 1 f t c')
-
-        # complex number multiplication
-
-        stft_repr = torch.view_as_complex(stft_repr)
-        mask = torch.view_as_complex(mask)
-
-        stft_repr = stft_repr * mask
-
-        # istft
-
-        stft_repr = rearrange(stft_repr, 'b n (f s) t -> (b n s) f t', s=self.audio_channels)
-
-        recon_audio = torch.istft(stft_repr, **self.stft_kwargs, window=stft_window, return_complex=False)
-
-        recon_audio = rearrange(recon_audio, '(b n s) t -> b n s t', s=self.audio_channels, n=num_stems)
-
-        if num_stems == 1:
-            recon_audio = rearrange(recon_audio, 'b 1 s t -> b s t')
-
-        # if a target is passed in, calculate loss for learning
-
-        if not exists(target):
-            return recon_audio
-
-        if self.num_stems > 1:
-            assert target.ndim == 4 and target.shape[1] == self.num_stems
-
-        if target.ndim == 2:
-            target = rearrange(target, '... t -> ... 1 t')
-
-        target = target[..., :recon_audio.shape[-1]]  # protect against lost length on istft
-
-        loss = F.l1_loss(recon_audio, target)
-
-        multi_stft_resolution_loss = 0.
-
-        for window_size in self.multi_stft_resolutions_window_sizes:
-            res_stft_kwargs = dict(
-                n_fft=max(window_size, self.multi_stft_n_fft),  # not sure what n_fft is across multi resolution stft
-                win_length=window_size,
-                return_complex=True,
-                window=self.multi_stft_window_fn(window_size, device=device),
-                **self.multi_stft_kwargs,
-            )
-
-            recon_Y = torch.stft(rearrange(recon_audio, '... s t -> (... s) t'), **res_stft_kwargs)
-            target_Y = torch.stft(rearrange(target, '... s t -> (... s) t'), **res_stft_kwargs)
-
-            multi_stft_resolution_loss = multi_stft_resolution_loss + F.l1_loss(recon_Y, target_Y)
-
-        weighted_multi_resolution_loss = multi_stft_resolution_loss * self.multi_stft_resolution_loss_weight
-
-        total_loss = loss + weighted_multi_resolution_loss
-
-        if not return_loss_breakdown:
-            return total_loss
+        with torch.cuda.amp.autocast(enabled=True):
+            
+            x = self.fuse(x) # b f t d*2 -> b t f d
+    
+            x = self.final_norm(x) 
+    
+            num_stems = len(self.mask_estimators)
+    
+            mask = torch.stack([fn(x) for fn in self.mask_estimators], dim=1)
+            mask = rearrange(mask, 'b n t (f c) -> b n f t c', c=2)
+    
+            # modulate frequency representation
+    
+            stft_repr = rearrange(stft_repr, 'b f t c -> b 1 f t c')
+    
+            # complex number multiplication
+    
+            stft_repr = torch.view_as_complex(stft_repr)
+            mask = torch.view_as_complex(mask)
+    
+            stft_repr = stft_repr * mask
+    
+            # istft
+    
+            stft_repr = rearrange(stft_repr, 'b n (f s) t -> (b n s) f t', s=self.audio_channels)
+    
+            recon_audio = torch.istft(stft_repr, **self.stft_kwargs, window=stft_window, return_complex=False)
+    
+            recon_audio = rearrange(recon_audio, '(b n s) t -> b n s t', s=self.audio_channels, n=num_stems)
+    
+            if num_stems == 1:
+                recon_audio = rearrange(recon_audio, 'b 1 s t -> b s t')
+    
+            # if a target is passed in, calculate loss for learning
+    
+            if not exists(target):
+                return recon_audio
+    
+            if self.num_stems > 1:
+                assert target.ndim == 4 and target.shape[1] == self.num_stems
+    
+            if target.ndim == 2:
+                target = rearrange(target, '... t -> ... 1 t')
+    
+            target = target[..., :recon_audio.shape[-1]]  # protect against lost length on istft
+    
+            loss = F.l1_loss(recon_audio, target)
+    
+            multi_stft_resolution_loss = 0.
+    
+            for window_size in self.multi_stft_resolutions_window_sizes:
+                res_stft_kwargs = dict(
+                    n_fft=max(window_size, self.multi_stft_n_fft),  # not sure what n_fft is across multi resolution stft
+                    win_length=window_size,
+                    return_complex=True,
+                    window=self.multi_stft_window_fn(window_size, device=device),
+                    **self.multi_stft_kwargs,
+                )
+    
+                recon_Y = torch.stft(rearrange(recon_audio, '... s t -> (... s) t'), **res_stft_kwargs)
+                target_Y = torch.stft(rearrange(target, '... s t -> (... s) t'), **res_stft_kwargs)
+    
+                multi_stft_resolution_loss = multi_stft_resolution_loss + F.l1_loss(recon_Y, target_Y)
+    
+            weighted_multi_resolution_loss = multi_stft_resolution_loss * self.multi_stft_resolution_loss_weight
+    
+            total_loss = loss + weighted_multi_resolution_loss
+    
+            if not return_loss_breakdown:
+                return total_loss
 
         return total_loss, (loss, multi_stft_resolution_loss)
